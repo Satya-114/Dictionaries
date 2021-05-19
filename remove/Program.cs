baef420b0b6520f0ec177f2f3266e0b3592b74fa
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-19 19:47:06 +0530

    Add files via upload

---
+++ remove/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Remove_Dictionary
+{
+    class Program
+    {
+        
+            static public void Main()
+            {
+
+                // Creating a dictionary
+                // using Dictionary<TKey,TValue> class
+                Dictionary<int, string> My_dict =
+                             new Dictionary<int, string>();
+
+                // Adding key/value pairs in the 
+                // Dictionary Using Add() method
+                My_dict.Add(1123, "Welcome");
+                My_dict.Add(1124, "to");
+                My_dict.Add(1125, "GeeksforGeeks");
+
+                // Before Remove() method
+                foreach (KeyValuePair<int, string> ele in My_dict)
+                {
+                    Console.WriteLine("{0} and {1}",
+                                ele.Key, ele.Value);
+                }
+                Console.WriteLine();
+
+                // Using Remove() method 
+                My_dict.Remove(1123);
+
+                // After Remove() method
+                foreach (KeyValuePair<int, string> ele in My_dict)
+                {
+                    Console.WriteLine("{0} and {1}",
+                                ele.Key, ele.Value);
+                }
+                Console.WriteLine();
+
+
+                // Using Clear() method
+                My_dict.Clear();
+
+                Console.WriteLine("Total number of key/value " +
+                 "pairs present in My_dict:{0}", My_dict.Count);
+
+            }
+        }
+      
+      }
+  
