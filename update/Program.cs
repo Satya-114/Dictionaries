7ea7e703476be33a097e63af69a53ec45fbe1bb4
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-19 19:43:53 +0530

    Add files via upload

---
+++ update/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Update_dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> students = new Dictionary<int, string>{
+            { 1, "John" },
+            { 2, "Mrk" },
+            { 3, "Bill" }
+         };
+            Console.WriteLine($"Name of student having id 2: {students[2]}");
+            students[2] = "Mark";
+            Console.WriteLine($"Updated Name of student having id 2: {students[2]}");
+            Console.ReadLine();
+        }
+    }
+}
